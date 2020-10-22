0b40e25f42d91fe528610138ded40bfb08b76915
MedJawad <mohamedjawad.aatafay@gmail.com>
2020-10-22 22:48:36 +0100

    fixes

--- postgrator-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "migrationDirectory": "migrations",
-    "driver": "pg",
-    "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
-}
--- src/server.js
+++ src/server.js
@@ -1,59 +1,76 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const mocha = require('mocha')
-const chai = require('chai')
-const { Client } = require('pg')
-const client = new Client({
-  user: 'dunder_mifflin',
-  host: 'localhost',
-  database: 'fooddonation',
-  password: '12345',
-  port: 3211,
-})
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+// const { Client } = require('pg')
+// const client = new Client({
+//   user: 'dunder_mifflin',
+//   host: 'localhost',
+//   database: 'fooddonation',
+//   password: '12345',
+//   port: 3211,
+// })
+const { Client } = require("pg");
 
-const app = express()
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const app = express();
 
 // const morganOption = (process.env.NODE_ENV === 'production')
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = process.env.NODE_ENV === "production" ? "tiny" : "common";
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
-app.use(express.json())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
- const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
- app.get('/api/*', (req, res) => {
-   res.json({ok: true});
- });
- app.post('/api/charity', async (req, res) => {
- await client.connect()
-const {
-  firstName,
-  lastName,
-  email,
-  address1,
-  address2,
-  city,
-  state,
-  zipCode,
-  phoneNumber,
-  website,
-} = req.body;
-   //Insert into DB
-   client.query(`INSERT INTO charities (firstName,lastName,email,address1,address2,city,state,zipCode,phoneNumber,website) VALUES ('${firstName}','${lastName}','${email}','${address1}','${address2}','${city}','${state}','${zipCode}','${phoneNumber}','${website}');`, (err, res) => {
-     console.log("sdqsdqsdqqsdqsdqsdqsdqsdqsdqsdqsd")
-    console.log(err, res)
-    client.end()
-   });
-  res.status(201).json({ok: true});
+app.get("/api/charity", (req, res) => {
+  //Select from DB
+  client.query(`SELECT * FROM charities;`, (err, results) => {
+    if (err) return res.status(400).json({ ok: false });
+    console.log(err, results);
+    const { rows } = results;
+    return res.status(200).json({ rows });
+  });
 });
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.post("/api/charity", async (req, res) => {
+  const {
+    charityName,
+    firstName,
+    lastName,
+    email,
+    address1,
+    address2,
+    city,
+    state,
+    zipCode,
+    phoneNumber,
+    website,
+  } = req.body;
+  //Insert into DB
+  client.query(
+    `INSERT INTO charities (charityName,firstName,lastName,email,address1,address2,city,state,zipCode,phoneNumber,website) VALUES ('${charityName}','${firstName}','${lastName}','${email}','${address1}','${address2}','${city}','${state}','${zipCode}','${phoneNumber}','${website}');`,
+    (err, results) => {
+      console.log(err, results);
+      if (err) return res.status(400).json({ ok: false });
 
- module.exports = {app};
+      return res.status(201).json({ ok: true, results });
+    }
+  );
+});
+
+client
+  .connect()
+  .then((done) => {
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
+module.exports = { app };
